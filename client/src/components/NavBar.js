e2e3e8953097b57b13932861f22b6cf397869475
Ouzinvw <ouzinvw@live.com>
2023-11-30 17:24:42 -0500

    updated navbar styling & links

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Navbar, Nav, Button, Container } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
@@ -7,52 +7,50 @@ const navigate = useNavigate();
 const isLoggedIn = localStorage.getItem('token');
 
 const handleLogout = () => {
-  // Add logic to clear the user's session (e.g., remove the token)
   localStorage.removeItem('token');
-  // Add any other logout logic as needed
   navigate('/');
 };
 
 const logoStyle = {
-    marginLeft: '20px', // Adjust the value to set the desired left margin
+    marginLeft: '20px'
   };
 
+const linkStyle = {
+  marginLeft: 'auto',
+  marginRight: '20px'
+};
+
+const btnStyle = {
+  marginLeft: '6px'
+};
+
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/about" style={logoStyle}>
-        <img
-          src="../ww_logo.png"
-          width="273"
-          height="90"
-          className="d-inline-block align-top"
-          alt="Logo"
-        />
-      </Navbar.Brand>
-      <Nav className="ml-auto">
-        {isLoggedIn ? (
-          <Button variant="primary" onClick={handleLogout}>
-            Logout
-          </Button>
-        ) : (
-          <Link to="/">
-            <Button variant="primary">Login</Button>
-          </Link>
-        )}
+    <Navbar bg="light" data-bs-theme="dark">
+        <Navbar.Brand href="/about" style={logoStyle}>
+          <img
+            src="../ww_logo.png"
+            width="273"
+            height="90"
+            className="d-inline-block align-top"
+            alt="Logo"
+          />
+        </Navbar.Brand>
+        <Nav style={linkStyle}>
         {isLoggedIn && (
-          <Link to="/edit-profile">
-            <Button variant="secondary" className="ml-2">
-              Edit Profile
-            </Button>
-          </Link>
-        )}
-        {isLoggedIn && (
-          <Link to="/dashboard">
-            <Button variant="secondary" className="ml-2">
-              Dashboard
-            </Button>
-          </Link>
-        )}
-      </Nav>
+            <Nav.Link className='text-dark' href="/about">ABOUT</Nav.Link>
+          )}
+          {isLoggedIn && (
+            <Nav.Link className='text-dark' href="/dashboard">DASHBOARD</Nav.Link>
+          )}
+          {isLoggedIn && (
+            <Nav.Link className='text-dark' href="/edit-profile">PROFILE</Nav.Link>
+          )}
+          {isLoggedIn ? (
+            <Button style={btnStyle} variant="outline-primary" onClick={handleLogout}>LOGOUT</Button>
+          ) : (
+            <Link to="/"><Button variant="primary">LOGIN</Button></Link>
+          )}
+        </Nav>
     </Navbar>
   );
 };
